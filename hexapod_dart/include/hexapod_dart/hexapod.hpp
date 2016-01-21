c90959593355fb607b64429e9a564295839d19f7
Konstantinos Chatzilygeroudis <costashatz@gmail.com>
2016-01-21 17:58:45 +0100

    New structure

--- hexapod_dart/include/hexapod_dart/hexapod.hpp
+++ hexapod_dart/include/hexapod_dart/hexapod.hpp
@@ -3,30 +3,125 @@
 
 #include <dart/dart.h>
 #include <Eigen/Core>
+#include <string>
+#include <fstream>
+#include <streambuf>
 
 namespace hexapod_dart {
     class Hexapod {
     public:
         Hexapod() {}
-        Hexapod(std::string urdf_file, std::vector<int> broken_legs);
-        Hexapod(dart::dynamics::SkeletonPtr skeleton, std::vector<int> broken_legs);
 
-        std::shared_ptr<Hexapod> clone() const;
+        Hexapod(std::string urdf_file, std::vector<int> broken_legs) : _skeleton(_load_urdf(urdf_file))
+        {
+            assert(_skeleton != nullptr);
+            _set_broken(broken_legs);
+        }
 
-        dart::dynamics::SkeletonPtr skeleton();
+        Hexapod(dart::dynamics::SkeletonPtr skeleton, std::vector<int> broken_legs) : _skeleton(skeleton)
+        {
+            assert(_skeleton != nullptr);
+            _set_broken(broken_legs);
+        }
 
-        bool is_broken(int leg);
-        std::vector<int> broken_legs();
+        std::shared_ptr<Hexapod> clone() const
+        {
+            // safely clone the skeleton
+            _skeleton->getMutex().lock();
+            auto tmp_skel = _skeleton->clone();
+            _skeleton->getMutex().unlock();
+            auto hexapod = std::make_shared<Hexapod>();
+            hexapod->_skeleton = tmp_skel;
+            hexapod->_broken_legs = _broken_legs;
+            return hexapod;
+        }
 
-        Eigen::Vector3d pos();
-        Eigen::Vector3d rot();
-        Eigen::Vector6d pose();
+        dart::dynamics::SkeletonPtr skeleton()
+        {
+            return _skeleton;
+        }
+
+        bool is_broken(int leg)
+        {
+            for (size_t j = 0; j < _broken_legs.size(); j++) {
+                if (leg == _broken_legs[j]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        std::vector<int> broken_legs()
+        {
+            return _broken_legs;
+        }
+
+        Eigen::Vector3d pos()
+        {
+            auto pos_and_rot = _skeleton->getPositions();
+            return {pos_and_rot(3), pos_and_rot(4), pos_and_rot(5)};
+        }
+
+        Eigen::Vector3d rot()
+        {
+            auto pos_and_rot = _skeleton->getPositions();
+            return {pos_and_rot(0), pos_and_rot(1), pos_and_rot(2)};
+        }
+
+        Eigen::Vector6d pose()
+        {
+            auto pos_and_rot = _skeleton->getPositions();
+            Eigen::Vector6d tmp;
+            tmp << pos_and_rot(3), pos_and_rot(4), pos_and_rot(5), pos_and_rot(0), pos_and_rot(1), pos_and_rot(2);
+            return tmp;
+        }
 
     protected:
-        dart::dynamics::SkeletonPtr _load_urdf(std::string urdf_file);
+        dart::dynamics::SkeletonPtr _load_urdf(std::string urdf_file)
+        {
+            // Load file into string
+            std::ifstream t(urdf_file);
+            std::string str((std::istreambuf_iterator<char>(t)),
+                std::istreambuf_iterator<char>());
+            // Load the Skeleton from a file
+            dart::utils::DartLoader loader;
+            dart::dynamics::SkeletonPtr tmp_skel = loader.parseSkeletonString(str, "");
+            if (tmp_skel == nullptr)
+                return nullptr;
+            tmp_skel->setName("hexapod");
 
-        void _set_broken(const std::vector<int>& broken_legs);
-        void _remove_legs();
+            // Set joint limits/actuator types
+            for (size_t i = 1; i < tmp_skel->getNumJoints(); ++i) {
+                tmp_skel->getJoint(i)->setPositionLimitEnforced(true);
+                tmp_skel->getJoint(i)->setActuatorType(dart::dynamics::Joint::VELOCITY);
+            }
+            return tmp_skel;
+        }
+
+        void _set_broken(const std::vector<int>& broken_legs)
+        {
+            _broken_legs = broken_legs;
+            std::sort(_broken_legs.begin(), _broken_legs.end());
+            _remove_legs();
+        }
+
+        void _remove_legs()
+        {
+            std::vector<int> to_remove;
+            for (size_t i = 1; i < _skeleton->getNumJoints(); i += 3) {
+                std::vector<int>::iterator it = std::find(_broken_legs.begin(), _broken_legs.end(), (i - 1) / 3);
+                if (it != _broken_legs.end()) {
+                    to_remove.push_back(i);
+                }
+            }
+
+            for (size_t i = 0; i < to_remove.size(); i++) {
+                auto tmp = _skeleton->getJoint(to_remove[i] - i * 3)->getChildBodyNode();
+                tmp->removeAllCollisionShapes();
+                tmp->removeAllVisualizationShapes();
+                tmp->remove();
+            }
+        }
 
         dart::dynamics::SkeletonPtr _skeleton;
         std::vector<int> _broken_legs;
--- hexapod_dart/src/test.cpp
+++ hexapod_dart/src/test.cpp
@@ -14,7 +14,7 @@ int main()
     // ctrl = {0.75, 0.9, 0.7, 0.75, 0.9, 0, 1, 0.1, 0.35, 0.2, 0.1, 0.15, 0.95, 0.25, 0.15, 0, 0.15, 0.15, 0.85, 0.95, 0.6, 0.9, 1, 0.2, 0.9, 0.85, 0.8, 0.25, 0.6, 0.95, 1, 0, 0.6, 0.3, 0.85, 0.75};
     // ctrl = {0.05, 0.8, 0.1, 0.9, 0.7, 0.35, 0.9, 1, 0.3, 0.7, 0.05, 0.15, 0.95, 0.75, 0.7, 0.95, 0.6, 0.6, 0.75, 0.7, 0.95, 0.6, 0.3, 0.35, 1, 0.4, 0.65, 0.55, 0.25, 0.6, 0.9, 0.75, 0.4, 1, 0.7, 0.85};
 
-    hexapod_dart::HexapodDARTSimu simu(ctrl, global_robot);
+    hexapod_dart::HexapodDARTSimu<> simu(ctrl, global_robot);
     // for(int i=0;i<167;i++)
     //     simu.run(0.015, true);
 
@@ -24,6 +24,12 @@ int main()
     simu.run(5);
     std::cout << simu.covered_distance() << " " << simu.arrival_angle() << std::endl;
     std::cout << simu.energy() << std::endl;
+    std::vector<double> v;
+    simu.get_descriptor<hexapod_dart::descriptors::DutyCycle>(v);
+    for (size_t i = 0; i < v.size(); i++) {
+        std::cout << v[i] << " ";
+    }
+    std::cout << std::endl;
 
     global_robot.reset();
     return 0;
